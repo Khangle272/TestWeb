744372051f74ef767a110867082bf8c22cae5bd3
Khangle272 <172981433+Khangle272@users.noreply.github.com>
2024-07-11 13:43:26 +0700

    Add files via upload

--- js/zoomImg.js
+++ js/zoomImg.js
@@ -4,6 +4,7 @@ function showPopup() {
 
 document.querySelector('.popup-img span').onclick = function() {
     document.querySelector('.popup-img').style.display = 'none';
+    resetZoom();
 }
 
 var scale = 1;
@@ -12,11 +13,19 @@ var pointX = 0;
 var pointY = 0;
 var start = {x: 0, y: 0};
 var zoom = document.getElementById("zoom");
-// Cho người dùng máy tính
+var initialDistance = null;
+
 function setTransform() {
     zoom.style.transform = "translate(" + pointX + "px," + pointY + "px) scale(" + scale + ")";
 }
 
+function resetZoom() {
+    scale = 1;
+    pointX = 0;
+    pointY = 0;
+    setTransform();
+}
+//For Computer
 function onMouseDown(e) {
     e.preventDefault();
     start = {x: e.clientX - pointX, y: e.clientY - pointY};
@@ -41,29 +50,49 @@ function onWheel(e) {
     var ys = (e.clientY - pointY) / scale;
     var delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
     var previousScale = scale;
-    scale *= (delta > 0) ? 1.05 : 1 / 1.05; // Chỉnh Độ Nhạy Tại Đây
+    scale *= (delta > 0) ? 1.07 : 1 / 1.07; // Adjust for sensivity
     pointX -= xs * (scale - previousScale);
     pointY -= ys * (scale - previousScale);
     setTransform();
 }
-// Cho người dùng điện thoại
+
+//For Mobile
 function onTouchStart(e) {
-    if (e.touches.length == 1) {
+    if (e.touches.length === 1) {
         start = {x: e.touches[0].clientX - pointX, y: e.touches[0].clientY - pointY};
         panning = true;
+    } else if (e.touches.length === 2) {
+        panning = false; 
+        initialDistance = getDistance(e.touches[0], e.touches[1]);
     }
 }
 
 function onTouchMove(e) {
     e.preventDefault();
-    if (!panning || e.touches.length != 1) return;
-    pointX = (e.touches[0].clientX - start.x);
-    pointY = (e.touches[0].clientY - start.y);
-    setTransform();
+    if (e.touches.length === 1 && panning) {
+        pointX = (e.touches[0].clientX - start.x);
+        pointY = (e.touches[0].clientY - start.y);
+        setTransform();
+    } else if (e.touches.length === 2 && initialDistance) {
+        var currentDistance = getDistance(e.touches[0], e.touches[1]);
+        var deltaScale = currentDistance / initialDistance;
+        initialDistance = currentDistance;
+        scale *= deltaScale;
+        setTransform();
+    }
 }
 
-function onTouchEnd() {
+function onTouchEnd(e) {
+    if (e.touches.length < 2) {
+        initialDistance = null;
+    }
     panning = false;
+}
+
+function getDistance(touch1, touch2) {
+    var dx = touch1.clientX - touch2.clientX;
+    var dy = touch1.clientY - touch2.clientY;
+    return Math.sqrt(dx * dx + dy * dy);
 }
 
 zoom.addEventListener('mousedown', onMouseDown);
