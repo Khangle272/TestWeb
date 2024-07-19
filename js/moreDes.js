fc0663ed3f658daf5f0ffeb0bc5313d5cd8fb094
Khangle272 <172981433+Khangle272@users.noreply.github.com>
2024-07-19 13:56:30 +0700

    Add files via upload

---
+++ js/moreDes.js
@@ -0,0 +1,8 @@
+function moreDetails(){
+    document.querySelector('.moreDetailShow').style.display = 'block';
+    document.querySelector('.overlayDes').style.display = 'block';
+}
+function closeDes(){
+    document.querySelector('.moreDetailShow').style.display = 'none';
+    document.querySelector('.overlayDes').style.display = 'none';
+}
