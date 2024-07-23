0bf6604ce18983794782243ce9ac474293ff529b
Khangle272 <172981433+Khangle272@users.noreply.github.com>
2024-07-23 14:18:41 +0700

    Add files via upload

---
+++ js/search.js
@@ -0,0 +1,6 @@
+function closeSearch(){
+    document.querySelector('.search-nav').style.display = 'none';
+}
+function openSearch(){
+    document.querySelector('.search-nav').style.display = 'block';
+}
