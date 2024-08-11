31e56c2656d44b0601557889043d8fdf4aae869d
Khangle272 <172981433+Khangle272@users.noreply.github.com>
2024-08-11 18:26:48 +0700

    Delete js/overlay.js

--- js/overlay.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('check').addEventListener('change', function () {
-    document.querySelector('.overlay').style.display = this.checked ? 'block' : 'none';
-});
-document.querySelector('.overlay').addEventListener('click', function () {
-    document.getElementById('check').checked = false;
-    this.style.display = 'none';
-});
-
